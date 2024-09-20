265d3f9930feb9009a99d6c42ecac76977a5cf8d
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-09-20 18:11:26 -0300

    Add base project and Home

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, TextInput, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2024</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
+    </View>
+  );
+}
