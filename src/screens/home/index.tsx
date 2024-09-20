f9f6f33bc380435a0b74808099bb6ab751d0673e
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-09-20 19:00:27 -0300

    Finish home page static

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,18 +1,59 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, TextInput, View } from "react-native";
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { styles } from "./styles";
+import { Participant } from "../components/Participant";
 
 export default function Home() {
+  const participants = [
+    "Erico",
+    "Diego",
+    "Biro",
+    "Ana",
+    "Isa",
+    "Jack",
+    "Mayk",
+    "João",
+  ];
+
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!");
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Você clicou em remover o participante ${name}`);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2024</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {participants.map((participant) => (
+          <Participant
+            key={participant}
+            name={participant}
+            onRemove={() => handleParticipantRemove("Rodrigo")}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
